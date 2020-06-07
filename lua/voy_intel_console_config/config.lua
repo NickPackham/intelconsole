e78a286801e8aa9073c25852a6fad851d7e35908
Nick Packham <46549847+NickPackham@users.noreply.github.com>
2020-06-07 06:32:31 -0700

    Create config.lua

---
+++ lua/voy_intel_console_config/config.lua
@@ -0,0 +1,11 @@
+VOY_Intel_Console_Config = {}
+
+VOY_Intel_Console_Config_BuyerModel = "models/player/valley/lgn/shop_keeper/shop_keeper.mdl" -- Playermodel
+
+VOY_Intel_Console_Config_IntelConsoleModel = "models/kingpommes/starwars/misc/bridge_console4.mdl" -- Not Playermodel
+
+VOY_Intel_Console_Config_Payment = 200 -- Payment (Per peice of Intel turned in)
+
+VOY_Intel_Console_Config_Cooldown = 3600 -- Seconds Form (60 Minutes by default) (Cooldown for Data Consoles)
+
+VOY_Intel_Console_Config_Max = 3 -- Max amount of Intel you can recieve from a Console.
